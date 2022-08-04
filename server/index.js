f3c703e86afa79cbeb8c6dcf02537776c310e69c
a1747701 <a1747701@student.adelaide.edu.au>
2022-08-04 15:04:23 +0930

    initialised project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config({path: "./config.env"});
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+})
