c6589a3d9f71503f84636c3c475c7a796d6af742
beamingboy <vinaykumar1802961@gmail.com>
2022-08-15 12:45:56 +0930

    adding all the database related packages and all the required schema. Also creating the tests for login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
-
-// import Calendar from "./components/calendar/Calendar";
+import ShaeTest from "./components/ShaeTest/ShaeTest" ;
+import Calendar from "./components/calendar/Calendar";
 
 import "./App.css";
 // import Home from "./components/login/Home";
-// import Login from "./components/login/Login";
-import NavbarLogin from "./components/Navbar/NavbarLogin";
+import Login from "./components/authentication/Login";
+// import NavbarLogin from "./components/Navbar/NavbarLogin";
 
 import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
-      <Route  exact path="/" element={<NavbarLogin />} />
+      <Route  exact path="/" element={<Login />} />
 
-      {/* <Route path="/shae" element={<ShaeTest />} /> */}
-      {/* <Route path="/calendar" element={<Calendar />} /> */}
+      <Route path="/shae" element={<ShaeTest />}/>
+      <Route path="/calendar" element={<Calendar />} />
     </Routes>
     
   );
