3cf8c1361f959734bcafbfc5c800b2a457e95fd2
a1747701 <a1747701@student.adelaide.edu.au>
2022-08-19 10:09:28 +0930

    added code to serve react app (production) in server/index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,12 @@ mongoose.connect(process.env.DATABASE_CONNECT,()=>{
 });
 app.use(express.json());
 app.use(cors())
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});
+
 app.use('/app',routerUrls);
 
 app.listen(4000,() =>{
