c20aa1dc72dd8331e613fd6354fce5b74a99b5c1
a1747701 <a1747701@student.adelaide.edu.au>
2022-08-19 12:09:56 +0930

    added button from react bootstrap but have not disabled

--- client/src/components/ShaeTest/ShaeTest.js
+++ client/src/components/ShaeTest/ShaeTest.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ShaeCalendar from "../ShaeCalendar/ShaeCalendar";
+import Button from "react-bootstrap/Button";
 
 const ShaeTest = () => {
   return (
     <div className="App">
       <p>My app</p>
       <ShaeCalendar />
+      <Button>
+        Get Recommended Times
+      </Button>
     </div>
   );
 };
