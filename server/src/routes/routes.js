d164784ee550f839966e01429543e49509cc1e39
beamingboy <vinaykumar1802961@gmail.com>
2022-08-26 20:06:08 +0930

    Revertng back to aqmal merge request

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,24 +1,21 @@
-import studentRouter from './studentRouter';
-
-export{studentRouter};
 
 
-// const express = require('express')
-// const router = express.Router()
-// const testData = require('../models/loginModels')
+const express = require('express')
+const router = express.Router()
+const testData = require('../models/loginModels')
 
-// router.post('/shae', (request,response) => {
-//     const testing = new testData({
-//         name:request.body.name
+router.post('/shae', (request,response) => {
+    const testing = new testData({
+        name:request.body.name
 
-//     })  
-//     testing.save()
-//     .then(data => {
-//         response.json(data)
-//     })
-//     .catch(error => {
-//         response.json(error)
-//     })
-// });
+    })  
+    testing.save()
+    .then(data => {
+        response.json(data)
+    })
+    .catch(error => {
+        response.json(error)
+    })
+});
 
-// module.exports = router
+module.exports = router
