83fd65e34e0de81a934e77dee73e0f45af2e9d89
a1802961 <a1802961@student.adelaide.edu.au>
2022-10-17 15:09:12 +1030

    Creating the home page for instructions

---
+++ client/src/components/authentication/HomePages/Landing.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card  from 'react-bootstrap/Card';
+
+const Landing = () => {
+    return (
+        <>
+
+            <Card fluid  border="dark">
+            <Navbar bg="light" variant="light">
+                <Container>
+                <Navbar.Brand text="red" href="/">
+                Uni Timetable
+                </Navbar.Brand>
+                <Container fluid>
+                    <Row>
+                    <Col md={{ span: 2, offset: -1 }} >
+                        <Nav className="me-auto">
+                        <Nav.Link href="/about">About</Nav.Link>
+                        </Nav>
+                    </Col>
+                    </Row>
+                </Container>
+                </Container>
+            </Navbar>
+            </Card>   
+            <br />
+                
+            
+        </>
+    );
+}
+
+exportconst Landing;
