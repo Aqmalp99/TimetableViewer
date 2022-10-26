f55325c0a38d86b50ed4809f43b3ac777ea78cd2
a1747701 <a1747701@student.adelaide.edu.au>
2022-10-26 21:24:21 +1030

    signup matches database

--- server/src/routes/signUpRouter.js
+++ server/src/routes/signUpRouter.js
@@ -8,23 +8,26 @@ const pool = require("../db/dbCongif");
 signUpRouter.post('/signup', async(req, res) => {
     try {
        
-        const {username, fullname, email, password,role,notification} = req.body;
+        const {username, firstName, surname, email, password} = req.body;
+        console.log(req.body);
         let hashedPassword = await bcrypt.hash(password,10);
         const existingUser = await pool.query(
-            "SELECT * FROM users WHERE username = $1",[username]
+            "SELECT * FROM users WHERE uni_id = $1",[username]
         );
         // check if the user is already registered
         if (existingUser.rowCount === 0){
             //register new user
             const addUser = await pool.query(
                 `INSERT INTO users( 
+                    uni_id,
                     username, 
                     password, 
-                    role, 
-                    notification
+                    first_name, 
+                    surname,
+                    role
                     )
-                    VALUES($1,$2,$3,$4);`,
-                    [email,hashedPassword,role,notification] 
+                    VALUES($1,$2,$3,$4,$5, 'student');`,
+                    [username, email, hashedPassword, firstName, surname] 
             );
             res.status(200).json({status: "signup successfull"});
         } else{
